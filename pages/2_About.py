df93a9e86b52d5092183db42d1bf94d694e6b972
CarlouisFernando <149694720+CarlouisFernando@users.noreply.github.com>
2026-05-25 11:53:17 +0700

    Update 2_About.py

--- pages/2_About.py
+++ pages/2_About.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # auth check
 if "user_id" not in st.session_state:
-    st.warning("silakan login terlebih dahulu")
+    st.warning("Silahkan Login Terlebih Dahulu")
     st.stop()
 
 st.title("About")
@@ -13,14 +13,19 @@ with col1:
     st.image("image/untar png.png", width=150)
 
 st.write("""
-aplikasi ini dikembangkan untuk mendukung forecasting harga emas Antam
-menggunakan pendekatan:
+Aplikasi ini dikembangkan untuk mendukung proses forecasting harga emas Antam menggunakan beberapa pendekatan metode, yaitu:
+
 - SARIMAX
 - Hybrid SARIMAX-XGBoost
 - Hybrid SARIMAX-LightGBM
+
 """)
 
 st.subheader("Info Pembuat")
 st.markdown("""
 Carlouis Fernando Hariyadi (535220156) merupakan mahasiswa Universitas Tarumanagara jurusan Teknik Informatika yang berfokus pada bidang Data Science dan predictive analytics. Aplikasi ini dikembangkan untuk membantu investor maupun masyarakat umum dalam menentukan keputusan investasi emas melalui prediksi harga emas Antam berbasis data.
-Prediksi dilakukan menggunakan pendekatan multivariate time series dengan memanfaatkan beberapa indikator ekonomi, seperti kurs USD/IDR, harga XAU/USD, inflasi, dan BI Rate. Model yang digunakan meliputi SARIMAX, Hybrid SARIMAX-XGBoost, serta Hybrid SARIMAX-LightGBM untuk menghasilkan prediksi yang lebih akurat dan adaptif terhadap perubahan kondisi ekonomi.""")
+
+Prediksi dilakukan menggunakan pendekatan multivariate time series dengan memanfaatkan beberapa indikator ekonomi, seperti kurs USD/IDR, harga XAU/USD, inflasi, dan BI Rate. Model yang digunakan meliputi SARIMAX, Hybrid SARIMAX-XGBoost, serta Hybrid SARIMAX-LightGBM untuk menghasilkan prediksi yang lebih akurat dan adaptif terhadap perubahan kondisi ekonomi.
+
+Pengembangan aplikasi ini dilakukan di bawah bimbingan dosen Universitas Tarumanagara, yaitu Bapak Tri Sutrisno, S.Si., M.Sc. dan Bapak Irvan Lewenusa, S.Kom., M.Kom.
+""")
